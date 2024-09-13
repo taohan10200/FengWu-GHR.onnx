a965839a8931f70b76d1f2f1456c06c1c34a1f17
taohan10200 <taohan10200@163.com>
2024-09-13 12:44:43 +0800

    update

--- script/shanxi.sh
+++
@@ -1,5 +0,0 @@
-# /bin/bash
-initial_time=$1
-source activate /home/admin/miniconda3/envs/ghr
-python tools/shanxi/merge_pressure_surface.py  --initial_time=${initial_time}
-python tools/shanxi/interp_stationv2.py  --initial_time=${initial_time}
